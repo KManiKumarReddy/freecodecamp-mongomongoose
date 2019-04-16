8e68f1829e05ef4b8d7f7eeec09a87a80362482d
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2019-04-17 00:01:08 +0530

    added mongoose mongodb connection and dotenv

--- myApp.js
+++ myApp.js
@@ -6,12 +6,18 @@
 /** # MONGOOSE SETUP #
 /*  ================== */
 
+require("dotenv").config();
+
 /** 1) Install & Set up mongoose */
 
 // Add `mongodb` and `mongoose` to the project's `package.json`. Then require 
 // `mongoose`. Store your **mLab** database URI in the private `.env` file 
 // as `MONGO_URI`. Connect to the database using `mongoose.connect(<Your URI>)`
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+.catch((err) => console.log(err));
 
 /** # SCHEMAS and MODELS #
 /*  ====================== */
