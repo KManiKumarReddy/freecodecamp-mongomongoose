1897b8faa65f90a56ccae3065cad9fd4d428cb8a
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2019-04-17 08:56:12 +0530

    created person model

--- myApp.js
+++ myApp.js
@@ -19,6 +19,8 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
 .catch((err) => console.log(err));
 
+const Schema = mongoose.Schema;
+
 /** # SCHEMAS and MODELS #
 /*  ====================== */
 
@@ -44,7 +46,14 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
 
 // <Your code here >
 
-var Person /* = <Your Model> */
+var Person  = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
 
 // **Note**: GoMix is a real server, and in real servers interactions with
 // the db are placed in handler functions, to be called when some event happens
